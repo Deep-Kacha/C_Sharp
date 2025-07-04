a6094c5f40bb47b3f57f1e9f463e1fe7541d9977
Deep-Kacha <deepnkacha2256@gmail.com>
2025-07-04 13:40:20 +0530

    .

---
+++ DeepKacha_23SOECE11022/T1Q1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepKacha_23SOECE11022
+{
+    internal class T1Q1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
