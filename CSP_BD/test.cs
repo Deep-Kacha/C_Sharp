c8bae7b7448ec22efe3586f873325b21b07ad2c6
Deep-Kacha <deepnkacha2256@gmail.com>
2025-07-04 13:15:27 +0530

    Initial commit

---
+++ CSP_BD/test.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//*****
+//*****
+//*****
+//*****
+//*****
+namespace CSP_BD
+{
+    internal class test
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("*****");
+            }
+        }
+    }
+}
