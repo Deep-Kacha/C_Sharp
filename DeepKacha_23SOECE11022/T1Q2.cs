e46ebb979cadc57dbb4fd6b48e5338c5f8b2e823
Deep-Kacha <deepnkacha2256@gmail.com>
2025-07-07 19:18:01 +0530

    .

---
+++ DeepKacha_23SOECE11022/T1Q2.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepKacha_23SOECE11022
+{
+	internal class T1Q2
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Name: Deep Kacha");
+			Console.WriteLine("DOB: 22/05/2006");
+			Console.WriteLine("Address:Vinod main road");
+			Console.WriteLine("City: Rajkot");
+			Console.WriteLine("Pincode: 360 002");
+			Console.WriteLine("State: Gujarat");
+			Console.WriteLine("Country: India");
+			Console.WriteLine("Email: dkacha329@rku.ac.in");
+		}
+	}
+}
