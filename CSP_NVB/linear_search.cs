4e0454ce21f427d879b4d113c301fe533c9eeb98
Deep-Kacha <deepnkacha2256@gmail.com>
2025-07-08 11:24:03 +0530

    -

---
+++ CSP_NVB/linear_search.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSP_NVB
+{
+    internal class linear_search
+    {
+        public static void Main(String[] args)
+        {
+
+            int[] c = new int[5];
+            Console.WriteLine("Enter elements of array");
+
+            for (int i = 0; i<c.Length; i++)
+            {
+                c[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Enter element to seach : ");
+            int key = Int32.Parse(Console.ReadLine());
+            bool found = false;
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] == key)
+                {
+                    Console.WriteLine($"Element at index : {i}");
+                    found = true;
+                    break;
+
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Element not found");
+            }
+        }
+        }
+    }
